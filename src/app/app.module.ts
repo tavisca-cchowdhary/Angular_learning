bc3870ffd995880a3c8d605f47d34b5305d81c21
cchowdhary <cchowdhary@tavisca.in>
2020-01-02 14:52:45 +0530

    style binding and class binding done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
+import { CoursesService } from './courses.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    CourseComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/courses.component.ts
@@ -0,0 +1,39 @@
+import { CoursesService } from './courses.service';
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'courses',
+    template:`
+        <button class="btn btn-primary" [class.active]="isActive" [style.backgroundColor]="isActive ? 'blue':'white'">Save</button>
+    `
+    // template: `<h2>{{getTitle()}}</h2>
+    //            <ul>
+    //                 <li *ngFor="let course of courses">
+    //                     {{course}}
+    //                 </li>
+    //             </ul>
+    //             <img src="{{imageUrl}}"/>
+    //             <img [src]="imageUrl"/>
+    //             <table>
+    //                 <tr>
+    //                     <td [attr.colspan]="colSpan"></td>
+    //                 </tr>
+    //             </table>
+
+    //         `
+})
+export class CoursesComponent {
+    title="List of courses";
+    courses ;
+    imageUrl="http://lorempixel.com/400/200";
+    colSpan=2;
+    isActive=true;
+
+    constructor(service:CoursesService){
+        //let service=new CoursesService();
+        this.courses=service.getCourses();
+    }
+    getTitle(){
+        return this.title;
+    }
+}
